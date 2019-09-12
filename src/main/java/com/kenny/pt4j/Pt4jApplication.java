4a607c410ea83cc089a9e9bcb2219a6f3a13d6e4
Kenny Fang <fangyao@7vp.club>
2019-09-12 21:10:16 +0800

    Initial commit

---
+++ src/main/java/com/kenny/pt4j/Pt4jApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.pt4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pt4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pt4jApplication.class, args);
+    }
+
+}
