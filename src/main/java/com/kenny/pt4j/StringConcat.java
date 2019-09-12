4e36b756c8d7c060a2d50cf5fd624ca868d4783e
Kenny Fang <fangyao@7vp.club>
2019-09-12 21:21:14 +0800

    String concat testing

---
+++ src/main/java/com/kenny/pt4j/StringConcat.java
@@ -0,0 +1,40 @@
+package com.kenny.pt4j;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StringConcat {
+
+    public static void main(String[] args) {
+
+        String date = "2019-09-dd";
+        final String dateFormat = "yyyy-MM-dd";
+        long s1 = System.nanoTime();
+        String str1 = "date[" + date + "] is wrong format:" + dateFormat;
+        long e1 = System.nanoTime();
+        log.warn(str1);
+        log.debug("using + elapse: {}ns", (e1 - s1));
+
+        s1 = System.nanoTime();
+        String str2 = "date[".concat(date).concat("] is wrong format:").concat(dateFormat);
+        e1 = System.nanoTime();
+        log.warn(str2);
+        log.debug("using concat elapse: {}ns", (e1 - s1));
+
+        s1 = System.nanoTime();
+        String str3 = String.format("date[%s] is wrong format:%s", date, dateFormat);
+        e1 = System.nanoTime();
+        log.warn(str3);
+        log.debug("using String.format elapse: {}ns", (e1 - s1));
+
+        s1 = System.nanoTime();
+        log.warn("date[{}] is wrong format:{}", "date", "DateFormat.Y_M_D");
+        e1 = System.nanoTime();
+        log.debug("using logger elapse: {}ns", (e1 - s1));
+
+        s1 = System.nanoTime();
+        log.warn("date[" + "date" + "] is wrong format:" + "DateFormat.Y_M_D");
+        e1 = System.nanoTime();
+        log.debug("using (logger +) elapse: {}ns", (e1 - s1));
+    }
+}
